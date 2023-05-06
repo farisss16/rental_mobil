3b643bf507fe11600545a8adf005ed3ef4ee641a
farisss16 <farisanas81@gmail.com>
2023-05-07 02:50:27 +0700

    first commit

---
+++ application/views/customer/dashboard.php
@@ -0,0 +1,329 @@
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/supramk5.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <h4>Supra MK5</h4>
+            <ul class="post-info">
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/mazdamx5.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <h4>Mazda MX 5</h4>
+            <ul class="post-info">
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/mazdaarx7.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <h4>Mazda RX 7</h4>
+            <ul class="post-info">
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/dodgecharger.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <a href="fleet.html">
+              <h4>Dodge Charger</h4>
+            </a>
+            <ul class="post-info">
+              <li>
+                <h2></h2>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/dodgechallenger.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <a href="fleet.html">
+              <h4>Dodge Challenger</h4>
+            </a>
+            <ul class="post-info">
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/mustang.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <a href="fleet.html">
+              <h4>Ford Mustang</h4>
+            </a>
+            <ul class="post-info">
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/toyota86.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <a href="fleet.html">
+              <h4>Toyota GR 86</h4>
+            </a>
+            <ul class="post-info">
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Offers</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/rx7.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>dari</strong> <span>Rp.500.000</span> <strong>/weekend</strong>
+              <a href="offers.html">
+                <h4>Mazda RX7 2002</h4>
+              </a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="offers.html">View More</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/mazdamx5.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>dari</strong> <span>Rp.600.000</span> <strong>/weekend</strong>
+              <a href="offers.html">
+                <h4>Mazda MX5 2023</h4>
+              </a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="offers.html">View More</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/mustang.png" alt="">
+            </div>
+            <div class="down-content">
+              <strong>dari</strong> <span>Rp.400.000</span> <strong>/weekend</strong>
+              <a href="offers.html">
+                <h4>Ford Mustang</h4>
+              </a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                      <li><i class="fa fa-bullseye"></i></li>
+                      <li><a href="offers.html">View More</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="call-to-action">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="main-content">
+          <div class="row">
+            <div class="col-lg-8">
+              <span>Lorem ipsum dolor sit amet.</span>
+              <h4>Sed doloribus accusantium reiciendis et officiis.</h4>
+            </div>
+            <div class="col-lg-4">
+              <div class="main-button">
+                <a href="contact.html">Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Blog</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/blog-1-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="blog-details.html">
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+              </a>
+
+              <p>Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet
+                eleifend.</p>
+
+              <ul class="post-info">
+                <li><a href="#">John Doe</a></li>
+                <li><a href="#">10.07.2020 10:20</a></li>
+                <li><a href="#"><i class="fa fa-comments" title="Comments"></i> 12</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/blog-2-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="blog-details.html">
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+              </a>
+
+              <p>Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet
+                eleifend.</p>
+
+              <ul class="post-info">
+                <li><a href="#">John Doe</a></li>
+                <li><a href="#">10.07.2020 10:20</a></li>
+                <li><a href="#"><i class="fa fa-comments" title="Comments"></i> 12</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/blog-3-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="blog-details.html">
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+              </a>
+
+              <p>Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet
+                eleifend.</p>
+
+              <ul class="post-info">
+                <li><a href="#">John Doe</a></li>
+                <li><a href="#">10.07.2020 10:20</a></li>
+                <li><a href="#"><i class="fa fa-comments" title="Comments"></i> 12</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div class="blog-posts">
+  <div class="container">
+    <div class="sidebar-item comments">
+      <h2 class="text-center">Testimonials</h2>
+      <br>
+      <br>
+      <div class="content">
+        <ul>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-01.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>John Doe<span>10.07.2020</span></h4>
+              <p>Fusce ornare mollis eros. Duis et diam vitae justo fringilla condimentum eu quis leo. Vestibulum id
+                turpis porttitor sapien facilisis scelerisque. Curabitur a nisl eu lacus convallis eleifend posuere id
+                tellus.</p>
+            </div>
+          </li>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-02.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>Jane Smith<span>10.07.2020</span></h4>
+              <p>Nullam nec pharetra nibh. Cras tortor nulla, faucibus id tincidunt in, ultrices eget ligula. Sed vitae
+                suscipit ligula. Vestibulum id turpis volutpat, lobortis turpis ac, molestie nibh.</p>
+            </div>
+          </li>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-03.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>Kate Blue<span>10.07.2020</span></h4>
+              <p>Nullam nec pharetra nibh. Cras tortor nulla, faucibus id tincidunt in, ultrices eget ligula. Sed vitae
+                suscipit ligula. Vestibulum id turpis volutpat, lobortis turpis ac, molestie nibh.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <br>
+      <br>
+
+      <div class="row justify-content-md-center">
+        <div class="col-md-3">
+          <div class="main-button">
+            <a href="testimonials.html">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div style="height: 180px;"></div>
---
+++ application/views/templates_customer/footer.php
@@ -0,0 +1,25 @@
+<!-- Bootstrap core JavaScript -->
+<script src="<?= base_url('assets/assets_shop') ?>/vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/assets_shop') ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Additional Scripts -->
+<script src="<?= base_url('assets/assets_shop') ?>/js/custom.js"></script>
+<script src="<?= base_url('assets/assets_shop') ?>/js/owl.js"></script>
+<script src="<?= base_url('assets/assets_shop') ?>/js/slick.js"></script>
+<script src="<?= base_url('assets/assets_shop') ?>/js/isotope.js"></script>
+<script src="<?= base_url('assets/assets_shop') ?>/js/accordions.js"></script>
+
+<script language="text/Javascript">
+  cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+  function clearField(t) {                   //declaring the array outside of the
+    if (!cleared[t.id]) {                      // function makes it static and global
+      cleared[t.id] = 1;  // you could use true and false, but that's more typing
+      t.value = '';         // with more chance of typos
+      t.style.color = '#fff';
+    }
+  }
+</script>
+
+</body>
+
+</html>
